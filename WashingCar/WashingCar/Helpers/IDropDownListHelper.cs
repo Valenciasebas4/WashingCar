05c24b2b4323c12a99d8c685eeba1e1e577d0f58
Valenciasebas4 <valenciasebastian80@gmail.com>
2023-06-13 17:24:26 -0500

    Create DropDownList Services

---
+++ WashingCar/WashingCar/Helpers/IDropDownListHelper.cs
@@ -0,0 +1,11 @@
+﻿using WashingCar.DAL.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WashingCar.Helpers
+{
+    public interface IDropDownListHelper
+    {
+        Task<IEnumerable<SelectListItem>> GetDDLServicesAsync();
+
+    }
+}
---
+++ WashingCar/WashingCar/Services/DropDownListHelper.cs
@@ -0,0 +1,43 @@
+﻿using WashingCar.DAL;
+using WashingCar.DAL.Entities;
+using WashingCar.Helpers;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace WashingCar.Services
+{
+    public class DropDownListHelper : IDropDownListHelper
+    {
+        public readonly DataBaseContext _context;
+
+        public DropDownListHelper(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public async Task<IEnumerable<SelectListItem>> GetDDLServicesAsync()
+        {
+            List<SelectListItem> listServices = await _context.Services
+                .Select(c => new SelectListItem
+                {
+                    Text = c.Name,
+                    Value = c.Id.ToString(),
+                })
+                .OrderBy(c => c.Text)
+                .ToListAsync();
+
+            listServices.Insert(0, new SelectListItem
+            {
+                Text = "Seleccione un servicio...",
+                Value = Guid.Empty.ToString(),
+                Selected = true
+            });
+
+            return listServices;
+        }
+
+
+    }
+}
